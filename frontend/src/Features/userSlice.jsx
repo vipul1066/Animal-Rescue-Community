f2bb171a502fb1b8abac94ffc521632bce6e518f
vipul1066 <vipulkatale10@gmail.com>
2025-02-16 23:22:44 +0530

    first commit

---
+++ frontend/src/Features/userSlice.jsx
@@ -0,0 +1,38 @@
+// src/slices/userSlice.js
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    id: null,
+    name: null,
+    email: null,
+    password: null,
+    phoneNo: null,
+    role: null,
+    address: null,
+  },
+
+  reducers: {
+    login: (state, action) => {
+      return { ...state, ...action.payload };
+    },
+
+    logOut: () => {
+      return {
+        id: null,
+        name: null,
+        email: null,
+        password: null,
+        phoneNo: null,
+        role: null,
+        address: null,
+      };
+    },
+  },
+});
+
+export const { login, logOut } = userSlice.actions;
+export const selectUser = (state) => state.user;
+
+export default userSlice.reducer;
